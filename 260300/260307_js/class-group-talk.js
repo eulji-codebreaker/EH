468806cc463d531d23806213e6fd3a4c46f5ceed
CallmeEterHist <kjls515@gmail.com>
2026-03-08 19:21:35 +0900

    260308 practice

--- 260300/260307_js/class-group-talk.js
+++ 260300/260307_js/class-group-talk.js
@@ -1,12 +1,12 @@
-function Group(prof, max){
+function Group(prof=undefined, max=null){
     this.prof=prof;
     this.max=max;
     this.member=new Array(1);
 }
 const makeGroup={
-    prof:undefined,
-    max:null,
     initiallize:function(){this.member[0]=(this.prof);},
+    setProf:function(prof){this.prof=prof;this.initiallize();},
+    setMax:function(max){this.max=max;this.initiallize();},
     add:function(person){if(this.member.length<this.max+1)this.member.push(person);else console.log('정원을 초과했습니다.')},
     remove:function(index){
         if(index===0)console.log('교수님은 삭제할 수 없습니다.')
@@ -19,15 +19,21 @@ var math=new Group('이순신', 30);
 math.initiallize();
 var eng=new Group('오바마', 30);
 eng.initiallize();
+var pe=new Group();
 
 console.log(math);
 console.log(eng);
-for(let i=0;i<31;i++){eng.add(`student${i+1}`)};
+for(let i=0;i<32;i++){eng.add(`student${i+1}`)};
 console.log(eng.member);
 eng.remove(0);
 console.log(eng.member);
-eng.remove(30);
-console.log(eng.member);
 eng.remove(15);
 console.log(eng.member);
-console.table(eng);
+console.log(math.member);
+math.add('누구');
+console.log(math.member);
+
+console.log(pe);
+pe.setMax(30);
+pe.setProf('마동석');
+console.log(pe);
